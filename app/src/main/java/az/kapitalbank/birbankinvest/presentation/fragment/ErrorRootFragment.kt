e46b015710677b13c9cc7a96fe409fcd376e1164
Anton Gordeev <an.gordeev@andersenlab.com>
2023-09-08 21:49:00 +0300

    BIBA-127 Refactored markup

--- app/src/main/java/az/kapitalbank/birbankinvest/presentation/fragment/ErrorRootFragment.kt
+++ app/src/main/java/az/kapitalbank/birbankinvest/presentation/fragment/ErrorRootFragment.kt
@@ -2,14 +2,10 @@ package az.kapitalbank.birbankinvest.presentation.fragment
 
 import androidx.fragment.app.Fragment
 import az.kapitalbank.birbankinvest.R
-import az.kapitalbank.birbankinvest.core_utils.viewBinding
-import az.kapitalbank.birbankinvest.databinding.FragmentErrorRootBinding
 
 class ErrorRootFragment : Fragment(R.layout.fragment_error_root) {
 
     companion object {
         fun getNewInstance() = ErrorRootFragment()
     }
-
-    private val binding by viewBinding(FragmentErrorRootBinding::bind)
 }
